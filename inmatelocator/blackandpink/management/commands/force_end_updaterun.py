3a795a791099873261bd46c4609778e730898c15
Charlie DeTar <cfd@media.mit.edu>
2015-11-13 12:22:17 -0700

    Add force_end_updaterun management command

---
+++ inmatelocator/blackandpink/management/commands/force_end_updaterun.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from blackandpink.models import UpdateRun
+
+class Command(BaseCommand):
+    help = "Mark an UpdateRun as finished."
+    def handle(self, *args, **options):
+        ur = UpdateRun.objects.get_unfinished()
+        ur.finished = timezone.now()
+        ur.save()
