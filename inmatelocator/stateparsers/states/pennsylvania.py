d7c9b903eaef22e84777ef152cc334f74d3b58d5
Charlie DeTar <cfd@media.mit.edu>
2016-03-09 09:36:00 -0700

    Rewrite PA search

--- inmatelocator/stateparsers/states/pennsylvania.py
+++ inmatelocator/stateparsers/states/pennsylvania.py
@@ -1,6 +1,6 @@
 import re
 import requests
-import lxml.html
+import json
 
 from stateparsers.states import BaseStateSearch
 from facilities.models import Facility
@@ -8,76 +8,50 @@ from facilities.models import Facility
 class Search(BaseStateSearch):
     administrator_name = "Pennsylvania"
     minimum_search_terms = [["last_name"], ["first_name"], ["number"]]
-    url = "http://inmatelocator.cor.state.pa.us/inmatelocatorweb/"
+    url = "http://inmatelocator.cor.pa.gov/api/inmLoc/firstCall"
 
     def crawl(self, **kwargs):
 
-        # Get the session cookie.
-        res = self.session.get(self.url)
-        root = lxml.html.fromstring(res.text)
-
         params = {
-           'txtLstNm': kwargs.get('last_name'),
-           'txtFrstNm': kwargs.get('first_name'),
-           'txtInmNo': kwargs.get('number'),
-       }
-
-        data = {
-            'txtMidNm': '',
-            'cboSex': '---',
-            'cboRace': '---',
-            'cboCommCnty': '---',
-            'cboLocation': '---',
-            'cboCitizenship': '---',
-            'grpAgeDOB': 'radDOB',
-            'txtDobAge': '',
-            'radList': 'radNam',
-            'btnSearch': 'Find+Inmate'
+            "id": kwargs.get('number', ""),
+            'lastName': kwargs.get('last_name', ""),
+            'firstName': kwargs.get('first_name', ""),
         }
-        data.update(params)
 
-        for key in ("__EVENTVALIDATION", "__VIEWSTATE", "__VIEWSTATEGENERATOR",
-                    "__EVENTTARGET", "__EVENTARGUMENT"):
-            data[key] = "".join(root.xpath('//input[@id="{}"]/@value'.format(key))) 
+        post_data = {
+            "middleName": "",
+            "countylistkey": "---",
+            "citizenlistkey": "---",
+            "racelistkey": "---",
+            "sexlistkey": "---",
+            "locationlistkey": "---",
+            "sortBy": "1"
+        }
+        post_data.update(params)
 
-        res = self.session.post(self.url, data)
-        root = lxml.html.fromstring(res.text)
-        rows = root.xpath("//tr[@class='clstdfield']")
-        results = []
-        errors = []
-        for row in rows:
-            texts = [' '.join(td.xpath('.//text()')).strip() for td in row.xpath('./td')]
-            texts = [re.sub('\s+', ' ', part) for part in texts]
-            number, first_name, middle_name, last_name, suffix, race, dob, current_location, committing_county = texts
-            name = " ".join((n for n in (first_name, middle_name, last_name) if n))
-            if suffix:
-                name = "{}, {}".format(name, suffix)
+        res = self.session.post(self.url, json=post_data)
+        if res.status_code != 200:
+            self.errors.append(res.content)
+            return
 
-            # filter also-known-as results.
-            skip = False
-            for name_part in ('first_name', 'last_name'):
-                if name_part in kwargs:
-                    if kwargs[name_part].lower() not in name.lower():
-                        skip = True
-                        break
-            if skip:
-                continue
+        results = json.loads(json.loads(res.text))
 
-
+        for result in results.get("inmates", []):
+            print(result)
+            name_part_keys = ["inm_firstname", "inm_middlename", "inm_lastname", "inm_suffix"]
+            name_parts = [result.get(key) for key in name_part_keys]
+            name = " ".join([a for a in name_parts if a])
+            raw_facility_name = result.get('fac_name')
             self.add_result(
                 name=name,
-                numbers={"": number},
+                numbers={"": result.get("inmate_number")},
                 search_terms=params,
-                raw_facility_name=current_location,
+                raw_facility_name=raw_facility_name,
                 status=self.STATUS_INCARCERATED,
-                facilities=Facility.objects.find_by_name("Pennsylvania", current_location),
+                facilities=Facility.objects.find_by_name("Pennsylvania", raw_facility_name),
                 extra=dict(
-                    race=race,
-                    date_of_birth=dob,
-                    committing_county=committing_county
+                    race=result.get('race'),
+                    date_of_birth=result.get('dob'),
+                    cnty_name=result.get('cnty_name')
                 )
             )
-
-
-
-
